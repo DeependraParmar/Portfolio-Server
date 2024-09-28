f3fb5f3fd966b64e4f95a10657472aca26c744bf
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-28 07:31:53 +0530

    Added index.js and setup of basic app

---
+++ index.js
@@ -0,0 +1,88 @@
+import { config } from "dotenv";
+import express from "express";
+import { connectDB } from "./libs/connectDB.js";
+import requestIp from "request-ip";
+import { ipMiddleware } from "./middleware.js";
+import Request from "./models/Request.js";
+import Like from "./models/Like.js";
+
+const app = express();
+
+config({
+    path: "./config/config.env"
+})
+
+connectDB();
+app.use(requestIp.mw());
+app.use(express.json());
+
+app.get("/add-request", ipMiddleware , async(req, res) => {
+    try{
+        const { ip } = req;
+
+        const pageVisits = await Request.countDocuments({});
+        const doIpExist = await Request.findOne({ ip });
+
+        if (!doIpExist) {
+            await Request.create({ ip });
+        }
+        
+
+        return res.json({
+            success: true,
+            message: "IP Recorded Successfully",
+            pageVisits
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.get("/like", ipMiddleware, async(req, res) => {
+    try{
+        const { ip } = req;
+        const isLiked = await Like.findOne({ ip });
+        
+        if (!isLiked) {
+            await Like.create({ ip });
+        }
+        
+        const totalLikes = await Like.countDocuments({});
+
+        return res.json({
+            success: true,
+            message: "Glad you liked it. 😍",
+            likes: totalLikes
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.get("/dislike", ipMiddleware, async(req, res) => {
+    try{
+        const { ip } = req;
+        const isLiked = await Like.findOne({ ip });
+        
+        if (isLiked) {
+            await Like.findOneAndDelete({ _id: isLiked._id });
+        }
+
+        const totalLikes = await Like.countDocuments({});
+        
+        return res.json({
+            success: true,
+            message: "Sad to see you go. 😞",
+            likes: totalLikes
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server up at port: ${process.env.PORT}`);
+});
